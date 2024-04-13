81c07a35826d28ff879ecf88a04bd09d6933e5c1
vanshgoel104203 <116905201+vanshgoel104203@users.noreply.github.com>
2024-04-14 01:42:50 +0530

    Update API.js

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://blood-bank-mern-rho.vercel.app/api/v1" });
+const API = axios.create({ baseURL: "https://blood-bank-mern-mu.vercel.app/api/v1" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("token")) {
