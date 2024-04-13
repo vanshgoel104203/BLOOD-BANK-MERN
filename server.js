de7055e4c775c4483b49322bb34d6c9be28c5260
vanshgoel104203 <116905201+vanshgoel104203@users.noreply.github.com>
2024-04-14 01:41:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use("/", router);
 const PORT = process.env.PORT || 8080;
 const Pass = process.env.Pass;
 //listen
-app.listen(PORT, () => {
+app.listen("https://blood-bank-mern-mu.vercel.app", () => {
   console.log(
     `Node Server Running In ${process.env.DEV_MODE} ModeOn Port ${process.env.PORT}`
       .bgBlue.white
